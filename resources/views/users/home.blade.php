9251480f44b02d5c2f31b16c04d85ac253cca8e9
kurtjohn21 <kjmojado21@gmail.com>
2024-04-04 20:56:40 +0800

    init upload

---
+++ resources/views/users/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title','Home')
+
+@section('content')
+    <div class="row">
+        <div class="col-8 bg-warning">
+            posts
+        </div>
+        <div class="col-4 bg-secondary">
+            suggested users
+        </div>
+    </div>
+@endsection
