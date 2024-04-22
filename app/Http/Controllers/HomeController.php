9560630e3ada934d57a75872485d758645734976
kurtjohn21 <kjmojado21@gmail.com>
2024-04-22 19:01:29 +0800

    suggested users and filter posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 class HomeController extends Controller
+
 {
     /**
      * Create a new controller instance.
@@ -30,8 +33,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $all_posts = $this->post->latest()->get();
+
+        $all_posts = $this->getHomePost();
+        $suggested_users = $this->getSuggestedUsers();
         return view('users.home')
-            ->with('all_posts', $all_posts);
+            ->with('all_posts', $all_posts)
+            ->with('suggested_users', $suggested_users);
+    }
+    public function getHomePost(){
+        $all_posts = $this->post->latest()->get();
+        $home_posts = [];
+        foreach($all_posts as $post){
+            if($post->user->isFollowed() || $post->user->id == Auth::id()){
+                $home_posts [] = $post;
+            }
+        }
+
+        return $home_posts;
+    }
+    public function getSuggestedUsers(){
+        $all_users = User::all()->except(Auth::id());
+        $suggested_users = [];
+        foreach($all_users as $user){
+            if(!$user->isFollowed() ){
+                $suggested_users[] = $user;
+            }
+        }
+        return $suggested_users;
     }
 }
--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -22,8 +22,41 @@
                 </div>
             @endforelse
         </div>
-        <div class="col-4 bg-secondary">
-            suggested users
+        <div class="col-4 ">
+            @if ($suggested_users)
+                <div class="row">
+                    <div class="col-auto">
+                        <p class="fw-bold text-secondary">Suggestions For You</p>
+                    </div>
+                    <div class="col text-end">
+                        <a href="#" class="fw-bold text-dark text-decoration-none">
+                            See all
+                        </a>
+                    </div>
+                </div>
+                @foreach ($suggested_users as $user)
+                    <div class="row align-items-center mb-3">
+                        <div class="col-auto">
+                            <a href="{{ route('profile.show', $user->id) }}">
+                                @if ($user->avatar)
+                                    <img src="{{ $user->avatar }}" alt="" class="rounded-circle avatar-sm">
+                                @else
+                                    <i class="fa-solid fa-user-circle text-secondary icon-sm"></i>
+                                @endif
+                            </a>
+                        </div>
+                        <div class="col ps-0 text-truncate">
+                            <a href="" class="text-decoration-none">{{ $user->name }}</a>
+                        </div>
+                        <div class="col text-end">
+                            <form action="" method="post">
+                                @csrf
+                                <button type="submit" class="btn border-0 p-0 text-primary btn-sm">Follow</button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
